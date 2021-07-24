98a48650ab40545884eae350b73852e8089a8174
poolug <pauluzcategui43@gmail.com>
2021-07-23 22:17:37 -0400

    update form, delete acces

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-http_basic_authenticate_with name: "pool", password: "123"
-
   def create
     @posts = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content]) #create lo envía directo a la db
   end
